4f7b198b13fd4cd02f63b4103eee8d24b3f07970
Milagros Mouriño <milagros2706m@gmail.com>
2021-10-12 16:52:43 +0200

    Modified mips1.as

--- mips1.asm
+++ mips1.asm
@@ -6,27 +6,29 @@
 	e: .space 4
 .text
 	la $t0, a
+	addi $t1, $zero, 1
 	lw $s0, 0($t0)
 	lw $s1, 4($t0)
 	lw $t6, 4($t0)
-	addi $t1, $zero, 1
 Potencia: 
 	beq $t6, $zero, FinBucle
-	mul $t1, $t1, $s0,
+	mul $t1, $t1, $s0
 	subi $t6, $t6, 1
 	j Potencia
 FinBucle: 
 	sw $t1, 8($t0)
-Suma: 
+Suma:
 	add $t2, $s0, $s1
 	add $t2, $t2, $t1
-	sw $t2, 12($t0)
+	sub $t6, $t2, $zero
 	addi $s2, $s2, 4
-MulCuatro: 
-	rem $t3, $t2, $s2
-	subi $t2, $t2, 1
-	bne $t3, $zero, MulCuatro
-	addi $t2, $t2, 1
-	sw $t2, 16($t0)
+MulCuatro:
+    rem $t3, $t2, $s2
+    beq $t3, $zero, Fin
+    subi $t2, $t2, 1
+    j MulCuatro
+Fin: addi $t2, $t2, 1
+    sw $t6, 12($t0)
+    sw $t2, 16($t0)
 	li $v0, 10
 	syscall
